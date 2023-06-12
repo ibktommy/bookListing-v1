e6c21a8bb9a086e98969476621641a948671af62
ibktommy <atomquartz@gmail.com>
2023-06-12 12:24:56 +0100

    First commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const bookData = [
+  {
+    id: 1,
+    title: "The Chronicles of Narnia",
+    image: "./images/narnia.jpg",
+  },
+
+  {
+    id: 2,
+    title: "The Psychology of Money",
+    image: "./images/moneyPsycho.jpg",
+  },
+
+  {
+    id: 3,
+    title: "Milk and Honey",
+    image: "./images/milkNhoney.jpg",
+  },
+
+  {
+    id: 4,
+    title: "Holy Quran",
+    image: "./images/quran.jpg",
+  },
+
+  {
+    id: 5,
+    title: "Holy Bible",
+    image: "./images/bible.jpg",
+  }
+]
+
+export default bookData;
