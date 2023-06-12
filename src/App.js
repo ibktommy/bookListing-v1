c851d582c453b537856cfc3540473778ca449ab6
ibktommy <atomquartz@gmail.com>
2023-06-12 13:37:26 +0100

    Pass the booklist-data to a state and create a function that deletes all the book-items when clear-all-button is clicked

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import BookListingItem from './components/BookListItem';
-import bookData from './data.js'
-
-let bookDataTotal = bookData.length
+import { useState } from "react";
+import BookListingItem from "./components/BookListItem";
+import bookData from "./data.js";
 
 const App = () => {
+	const [bookList, setBookList] = useState(bookData);
+
+	let bookDataTotal = bookList.length;
+
+	function clearBookList () {
+		setBookList([])
+	}
+
 	return (
 		<div className="container">
 			<div className="main-box">
 				<h4 className="header">{bookDataTotal} total books</h4>
-				{bookData.map((bookItem, id) => (
+				{bookList.map((bookItem, id) => (
 					<BookListingItem key={id} bookItemProps={bookItem} />
 				))}
-				<button className="book-submit">Clear All</button>
+				<button className="book-submit" onClick={clearBookList}>Clear All</button>
 			</div>
 		</div>
 	);
