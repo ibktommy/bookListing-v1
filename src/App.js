806f37c1800d077bf49080179c6fea84b700aa00
ibktommy <atomquartz@gmail.com>
2023-06-12 13:07:13 +0100

    Style the App

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import bookData from './data.js'
 
 let bookDataTotal = bookData.length
 
-
 const App = () => {
 	return (
 		<div className="container">
 			<div className="main-box">
-				<h4>{bookDataTotal} total books</h4>
-				{
-					bookData.map((bookItem, id) => (
-						<BookListingItem key={id} bookItemProps={bookItem}/>
-					))
-				}
-				<button className="submit">Clear All</button>
+				<h4 className="header">{bookDataTotal} total books</h4>
+				{bookData.map((bookItem, id) => (
+					<BookListingItem key={id} bookItemProps={bookItem} />
+				))}
+				<button className="book-submit">Clear All</button>
 			</div>
 		</div>
 	);
