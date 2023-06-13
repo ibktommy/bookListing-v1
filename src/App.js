4044b2771deb0646225631334737b64e468b4a82
ibktommy <atomquartz@gmail.com>
2023-06-13 12:53:06 +0100

    Create form-inputs to accept book-title and book-author and then add them to an object that will added to the top of the book-list-array

--- src/App.js
+++ src/App.js
@@ -4,25 +4,80 @@ import bookData from "./data.js";
 
 const App = () => {
 	const [bookList, setBookList] = useState(bookData);
+	const [title, setTitle] = useState("");
+	const [author, setAuthor] = useState("");
+	const [image, setImage] = useState("");
 
 	let bookDataTotal = bookList.length;
 
 	// Function that deletes all book in the bookList
-	function clearBookList () {
-		setBookList([])
+	function clearBookList() {
+		setBookList([]);
 	}
 
 	// Function that checks for the book selected and deletes it from the bookList
-	function deleteOneItem (index) {
-		setBookList(bookList.filter((bookItem) => bookItem.id !== index))
+	function deleteOneItem(index) {
+		setBookList(bookList.filter((bookItem) => bookItem.id !== index));
 	}
 
 	function resetList() {
 		setBookList(bookData);
 	}
 
+	function getTitle(e) {
+		let titleValue = e.target.value.trim();
+		setTitle(titleValue);
+	}
+
+	function getAuthor(e) {
+		let authorValue = e.target.value.trim();
+		setAuthor(authorValue);
+	}
+
+	function handleFormSubmit(e) {
+		e.preventDefault();
+
+		if (title === "" || author === "") {
+			alert("Please fill the input fields");
+			return;
+		}
+
+		let newBookItem = {
+			title: title,
+			author: author,
+		};
+
+		console.log(newBookItem);
+
+		setBookList([newBookItem, ...bookList])
+
+		setAuthor("")
+		setTitle("")
+	}
+
 	return (
 		<div className="container">
+			<form id="book-form" onSubmit={handleFormSubmit}>
+				<input
+					type="text"
+					name="book-title"
+					id="book-title"
+					placeholder="Book Title"
+					value={title}
+					onChange={getTitle}
+				/>
+				<input
+					type="text"
+					name="book-name"
+					id="book-name"
+					placeholder="Book Name"
+					value={author}
+					onChange={getAuthor}
+				/>
+				<input type="file" name="book-pic" id="book-pic" />
+				<button className="form-button">Add Book</button>
+			</form>
+
 			<div className="main-box">
 				<h4 className="header">{bookDataTotal} total books</h4>
 				{bookList.map((filteredBookItem, id) => (
